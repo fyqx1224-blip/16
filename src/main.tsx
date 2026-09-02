486873fc0d95c9072c73d9670aae5b87bc029ec7
fyqx1224-blip <fyqx1224@gmail.com>
2026-09-02 08:02:05 -0300

    Build observer registration prologue

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from '../app/page';
 import '../app/globals.css';
-import '../app/extreme.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><Home /></React.StrictMode>,
